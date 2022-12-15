0e06e3fabbd8d8eb47db1ccfabba149f47769249
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-12-15 17:41:46 +0100

    Adding basic run script

---
+++ src/main/java/org/matsim/run/runDrtScenario.java
@@ -0,0 +1,71 @@
+package org.matsim.run;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.application.MATSimApplication;
+import org.matsim.contrib.drt.optimizer.insertion.IncrementalStopDurationEstimator;
+import org.matsim.contrib.drt.routing.DrtRoute;
+import org.matsim.contrib.drt.routing.DrtRouteFactory;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtConfigs;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.drt.run.MultiModeDrtModule;
+import org.matsim.contrib.drt.schedule.StopDurationEstimator;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeModule;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.utils.LinearDrtStopDurationEstimator;
+import picocli.CommandLine;
+
+import javax.annotation.Nullable;
+
+@CommandLine.Command(header = ":: Run insertion strategy ::", version = runDrtScenario.VERSION)
+public class runDrtScenario extends MATSimApplication {
+    static final String VERSION = "1.0";
+
+    public static void main(String[] args) {
+        MATSimApplication.run(runDrtScenario.class, args);
+    }
+
+    @Nullable
+    @Override
+    protected Config prepareConfig(Config config) {
+        MultiModeDrtConfigGroup multiModeDrtConfig = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
+        ConfigUtils.addOrGetModule(config, DvrpConfigGroup.class);
+        DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfig, config.planCalcScore(), config.plansCalcRoute());
+        return config;
+    }
+
+    @Override
+    protected void prepareScenario(Scenario scenario) {
+        scenario.getPopulation()
+                .getFactory()
+                .getRouteFactories()
+                .setRouteFactory(DrtRoute.class, new DrtRouteFactory());
+    }
+
+    @Override
+    protected void prepareControler(Controler controler) {
+        Config config = controler.getConfig();
+        MultiModeDrtConfigGroup multiModeDrtConfig = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
+        controler.addOverridingModule(new DvrpModule());
+        controler.addOverridingModule(new MultiModeDrtModule());
+        controler.configureQSimComponents(DvrpQSimComponents.activateAllModes(multiModeDrtConfig));
+
+        MultiModeDrtConfigGroup multiModeDrtConfigGroup = MultiModeDrtConfigGroup.get(config);
+        for (DrtConfigGroup drtCfg : multiModeDrtConfigGroup.getModalElements()) {
+            // Add linear stop duration module
+            controler.addOverridingModule(new AbstractDvrpModeModule(drtCfg.getMode()) {
+                @Override
+                public void install() {
+                    bindModal(StopDurationEstimator.class).toInstance((vehicle, dropoffRequests, pickupRequests) -> drtCfg.stopDuration * (dropoffRequests.size() + pickupRequests.size()));
+                    bindModal(IncrementalStopDurationEstimator.class).toInstance(new LinearDrtStopDurationEstimator(drtCfg.stopDuration));
+                }
+            });
+        }
+    }
+
+}
